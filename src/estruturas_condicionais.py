e4ef1aa4d959561e7c4bfbfb02c9cdebcd979ee5
Roger Rezende <roger.rezendegmail.com>
2020-10-15 20:57:29 -0300

    criação da função para verificar se vai gerar a raiz quadrada de um número ou não

--- src/estruturas_condicionais.py
+++ src/estruturas_condicionais.py
@@ -1,5 +1,17 @@
+import math
+
+
 def maior_numero(a, b):
     if a > b:
         return a
     else:
         return b
+
+
+def raiz_quadrada_ou_numero_invalido(a):
+    if a > 0:
+        return math.sqrt(a)
+    elif a < 0:
+        return "Numero Invalido"
+    else:
+        return 0
